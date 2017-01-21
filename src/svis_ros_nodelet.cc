07c920a767832faa43f9e30f9dac100faf9d63aa
jakeware <jakeware@gmail.com>
2017-01-21 15:29:43 -0500

    Cleanup and rename.

--- src/svis_ros_nodelet.cc
+++ src/svis_ros_nodelet.cc
@@ -1,65 +1,39 @@
 // Copyright 2016 Massachusetts Institute of Technology
 
-#include <memory>
-#include <limits>
-#include <thread>
-#include <vector>
-
-#include <Eigen/Core>
-
-#include <sophus/se3.hpp>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 
 #include <ros/ros.h>
-
 #include <nodelet/nodelet.h>
 
-#include <ros_sensor_streams/tracked_image_stream.h>
+#include "hid.h"
 
-#include <stats_tracker/stats_tracker.h>
-
-#include <depthest/trackers/track_manager.h>
-#include <depthest/trackers/klt_feature_tracker.h>
-
-namespace dft = depthest::trackers;
-
-namespace depthest_ros {
+namespace svis_ros {
 
 /**
- * \brief Tracks sparse features using OpenCV KLT tracker.
+ * \brief A simple visual inertial synchronization approach.
  *
- * KLT tracker is essentially Lucas-Kanade optical flow, but with features that
- * are "good" for tracking.
- *
- * Derived from:
- * http://docs.opencv.org/3.1.0/d7/d8b/tutorial_py_lucas_kanade.html#gsc.tab=0
+ * This package contains the ROS portion of a Simple Visual Inertial Synchronization approach that accepts camera strobe messages * from the Teensy microcontroller and synchronizes them with camera image messages.
  */
-class FeatureTrackerNodelet : public nodelet::Nodelet {
+class SVISNodelet : public nodelet::Nodelet {
  public:
   /**
    * \brief Constructor.
    *
    * NOTE: Default, no-args constructor must exist.
    */
-  FeatureTrackerNodelet() = default;
+  SVISNodelet() = default;
 
-  virtual ~FeatureTrackerNodelet() {
-    if (thread_.joinable()) {
-      thread_.join();
-    }
+  ~SVISNodelet() = default;
+  
+  SVISNodelet(const SVISNodelet& rhs) = delete;
+  SVISNodelet& operator=(const SVISNodelet& rhs) = delete;
 
-    return;
-  }
-
-  FeatureTrackerNodelet(const FeatureTrackerNodelet& rhs) = delete;
-  FeatureTrackerNodelet& operator=(const FeatureTrackerNodelet& rhs) = delete;
-
-  FeatureTrackerNodelet(FeatureTrackerNodelet&& rhs) = delete;
-  FeatureTrackerNodelet& operator=(FeatureTrackerNodelet&& rhs) = delete;
+  SVISNodelet(SVISNodelet&& rhs) = delete;
+  SVISNodelet& operator=(SVISNodelet&& rhs) = delete;
 
   /**
    * \brief Nodelet initialization.
@@ -69,42 +43,10 @@ class FeatureTrackerNodelet : public nodelet::Nodelet {
    */
   virtual void onInit() {
     // Grab a handle to the parent node.
-    ros::NodeHandle& nh = getNodeHandle();
-    ros::NodeHandle& pnh = getPrivateNodeHandle();
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
 
-    // Grab some params.
-    if (!pnh.getParam("camera_world_frame_id", camera_world_frame_id_)) {
-      NODELET_ERROR("camera_world_frame_id must be specified!");
-      return;
-    }
-
-    float max_dropouts;
-    if (!pnh.getParam("max_dropouts", max_dropouts)) {
-      NODELET_ERROR("max_dropouts must be specified!");
-      return;
-    }
-
-    // Initialize tracker.
-    manager_ = std::make_shared<dft::TrackManager>();
-
-    dft::KLTFeatureTracker::Params params;
-    params.max_dropouts = max_dropouts;
-    tracker_ = std::make_shared<dft::KLTFeatureTracker>(manager_.get(), params);
-
-    // Setup input stream.
-    input_ = std::make_shared<ros_sensor_streams::
-                              TrackedImageStream>(camera_world_frame_id_, nh);
-
-    // Wait until input is initialized.
-    while (!input_->inited()) {
-      std::this_thread::yield();
-    }
-
-    Kinv_ = input_->K().inverse();
-
-    num_imgs_ = 0;
-
-    thread_ = std::thread(&FeatureTrackerNodelet::run, this);
+    Run();
 
     return;
   }
@@ -112,92 +54,50 @@ class FeatureTrackerNodelet : public nodelet::Nodelet {
   /**
    * \brief Main processing loop.
    */
-  void run() {
+  void Run() {
+    int i, r, num;
+    char buf[64];
+
+    // C-based example is 16C0:0480:FFAB:0200
+    r = rawhid_open(1, 0x16C0, 0x0480, 0xFFAB, 0x0200);
+    if (r <= 0) {
+      // Arduino-based example is 16C0:0486:FFAB:0200
+      r = rawhid_open(1, 0x16C0, 0x0486, 0xFFAB, 0x0200);
+      if (r <= 0) {
+        printf("no rawhid device found\n");
+        return;
+      }
+    }
+    printf("found rawhid device\n");
+
     while (ros::ok()) {
-      stats_.tick("run");
-
-      // Wait for queue to have items.
-      std::unique_lock<std::recursive_mutex> lock(input_->queue().mutex());
-      input_->queue().non_empty().wait(lock, [this](){
-          return (input_->queue().size() > 0);
-        });
-      lock.unlock();
-
-      // Grab the first item in the queue.
-      auto item = input_->queue().pop();
-
-      double time = std::get<0>(item);
-      cv::Mat rgb = std::get<1>(item);
-      Eigen::Quaternionf q = std::get<2>(item);
-      Eigen::Vector3f t = std::get<3>(item);
-
-      Sophus::SE3f pose(q, t);
-
-      // Eat data.
-      process(time, pose, rgb);
-
-      num_imgs_++;
-
-      stats_.tock("run");
-
-      NODELET_DEBUG("run = %3.1fms\n", stats_.timings("run"));
+      // check if any Raw HID packet has arrived
+      num = rawhid_recv(0, buf, 64, 220);
+      if (num < 0) {
+        printf("\nerror reading, device went offline\n");
+        rawhid_close(0);
+        return;
+      }
+      
+      if (num > 0) {
+        printf("\nrecv %d bytes:\n", num);
+        for (i=0; i<num; i++) {
+          printf("%02X ", buf[i] & 255);
+          if (i % 16 == 15 && i < num-1) printf("\n");
+        }
+        printf("\n");
+      }
     }
-
-    return;
-  }
-
-  void process(const double time, const Sophus::SE3f& pose,
-               const cv::Mat& img) {
-    stats_.tick("process");
-
-    // Convert image to grayscale, if necessary
-    cv::Mat img_gray;
-    if (img.channels() == 1) {
-      img_gray = img;
-    } else {
-      cv::cvtColor(img, img_gray, cv::COLOR_RGB2GRAY);
-    }
-
-    // Assert that we work with 8 bit images
-    assert(img_gray.elemSize() == 1);
-
-    // Track.
-    tracker_->track(num_imgs_, img_gray);
-
-    NODELET_DEBUG("num_tracks = %i\n",
-                  static_cast<int>(manager_->tracks().size()));
-
-    stats_.tock("process");
-
-    NODELET_DEBUG("process = %3.1fms\n", stats_.timings("process"));
 
     return;
   }
 
  private:
-  // Main processing thread.
-  std::thread thread_;
 
-  // Keeps track of statistics and timings.
-  stats_tracker::StatsTracker stats_;
-
-  // Number of images processed.
-  int num_imgs_;
-
-  // Frame id of the world in camera (Right-Down-Forward) coordinates.
-  std::string camera_world_frame_id_;
-
-  // Input stream object.
-  std::shared_ptr<ros_sensor_streams::TrackedImageStream> input_;
-  Eigen::Matrix3f Kinv_;
-
-  // Feature tracker.
-  std::shared_ptr<dft::TrackManager> manager_;
-  std::shared_ptr<dft::FeatureTracker> tracker_;
 };
 
-}  // namespace depthest_ros
+}  // namespace svis_ros
 
 // Export as a plugin.
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(depthest_ros::FeatureTrackerNodelet, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(svis_ros::SVISNodelet, nodelet::Nodelet)
