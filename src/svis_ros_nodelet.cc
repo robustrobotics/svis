d33e8fd94eabe58c3b4f96950c3d2eed18a83b68
jakeware <jakeware@gmail.com>
2017-01-23 00:59:07 -0500

    Partial cleanup.

--- src/svis_ros_nodelet.cc
+++ src/svis_ros_nodelet.cc
@@ -58,26 +58,33 @@ class SVISNodelet : public nodelet::Nodelet {
    * \brief Main processing loop.
    */
   void Run() {
-    int i, r, num;
-    char buf[64];
-
+    // open rawhid port
+    int r = rawhid_open(1, 0x16C0, 0x0486, 0xFFAB, 0x0200);
     // C-based example is 16C0:0480:FFAB:0200
     // Arduino-based example is 16C0:0486:FFAB:0200
-    r = rawhid_open(1, 0x16C0, 0x0486, 0xFFAB, 0x0200);
+
+    // check return
     if (r <= 0) {
       NODELET_ERROR("(svis_ros) No rawhid device found.\n");
       return;
+    } else {
+      NODELET_INFO("(svis_ros) Found rawhid device\n");
     }
-    NODELET_INFO("(svis_ros) Found rawhid device\n");
 
+    // loop
+    char buf[64];
     ros::Time t_now;
     ros::Time t_last;
     while (ros::ok()) {
       // check if any Raw HID packet has arrived
-      num = rawhid_recv(0, buf, 64, 220);
+      int num = rawhid_recv(0, buf, 64, 220);
+
+      // timing
       t_now = ros::Time::now();
       // NODELET_INFO("(svis_ros) [n: %i, dt: %f]", num, (t_now - t_last).toSec());
       t_last = t_now;
+
+      // check byte count
       if (num < 0) {
         NODELET_ERROR("(svis_ros): Error reading, device went offline");
         rawhid_close(0);
@@ -85,101 +92,27 @@ class SVISNodelet : public nodelet::Nodelet {
       } else if (num == 0) {
         NODELET_INFO("(svis_ros) 0");
       } else if (num > 0) {
-        // NODELET_INFO("(svis_ros) buffer: ");
-        // for (i = 0; i < num; i++) {
-        //   printf("%02X ", buf[i] & 255);
-        // }
-        // printf("\n");
-
-        int ind = 0;
-        int mask8  = 0x00000000000000FF;
-        int mask16 = 0x000000000000FFFF;
-        int mask32 = 0x00000000FFFFFFFF;
+        // debug print
+        if (print_buffer_) {
+          PrintBuffer(num, buf);
+        }
 
         // checksum
-        uint16_t checksum_calc = 0;
-        for (int i = 0; i < send_buffer_size - 2; i++) {
-          checksum_calc += buf[i] & 0xFF;
+        if(GetChecksum(ind, buf)) {
+          return;
         }
-        uint16_t checksum_orig = 0;
-        memcpy(&checksum_orig, &buf[checksum_index], sizeof(checksum_orig));
-
-        if (checksum_calc != checksum_orig) {
-          NODELET_INFO("(svis_ros) checksum error [%02X, %02X] [%02X, %02X]",
-                       buf[checksum_index] & 0xFF, buf[checksum_index + 1] & 0xFF, checksum_calc, checksum_orig);
-          continue;
-        }
-
+        
         // header
         header_packet header;
-
-        // send_count
-        header.send_count = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-        NODELET_INFO("(svis_ros) send_count: [%i, %i]", ind, header.send_count);
-        ind += 2;
-
-        // imu_packet_count
-        header.imu_count = mask8 & buf[ind];
-        NODELET_INFO("(svis_ros) imu_packet_count: [%i, %i]", ind, header.imu_count);
-        ind++;
-
-        // strobe_packet_count
-        header.strobe_count = mask8 & buf[ind];
-        NODELET_INFO("(svis_ros) strobe_packet_count: [%i, %i]", ind, header.strobe_count);
-        ind++;
+        GetHeader(buf, header);
 
         // imu
         std::vector<imu_packet> imu_packets(header.imu_count);
-        for (int i = 0; i < header.imu_count; i++) {
-          imu_packet imu;
-
-          // timestamp
-          memcpy(&imu.timestamp, &buf[ind], 4);
-          NODELET_INFO("(svis_ros) imu.timestamp: [%i, %i]", ind, imu.timestamp);
-          ind += 4;
-
-          // accel
-          imu.acc[0] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-          imu.acc[1] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-          imu.acc[2] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-
-          NODELET_INFO("(svis_ros) imu.acc: [%i, %i, %i]", imu.acc[0], imu.acc[1], imu.acc[2]);
-
-          // gyro
-          imu.gyro[0] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-          imu.gyro[1] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-          imu.gyro[2] = mask16 & (mask8 & buf[ind] | ((mask8 & buf[ind + 1]) << 8));
-          ind += 2;
-
-          NODELET_INFO("(svis_ros) imu.gyro: [%i, %i, %i]", imu.gyro[0], imu.gyro[1], imu.gyro[2]);
-
-          // save packet
-          imu_packets[i] = imu;
-        }
+        GetIMU(buf, header, imu_packets);
 
         // strobe
         std::vector<strobe_packet> strobe_packets(header.strobe_count);
-        for (int i = 0; i < header.strobe_count; i++) {
-          strobe_packet strobe;
-
-          // timestamp
-          memcpy(&strobe.timestamp, &buf[ind], 4);
-          NODELET_INFO("(svis_ros) strobe.timestamp: [%i, %i]", ind, strobe.timestamp);
-          ind += 4;
-
-          // count
-          strobe.count = mask8 & buf[ind];
-          NODELET_INFO("(svis_ros) strobe.count: [%i, %i]", ind, strobe.count);
-          ind++;
-
-          // save packet
-          strobe_packets[i] = strobe;
-        }
+        GetStrobe(buf, header, strobe_packets);
 
         // new line
         printf("\n");
@@ -191,6 +124,109 @@ class SVISNodelet : public nodelet::Nodelet {
   }
 
  private:
+  int GetChecksum(std::unique_ptr<char> buf) {
+    // calculate checksum
+    uint16_t checksum_calc = 0;
+    for (int i = 0; i < send_buffer_size - 2; i++) {
+      checksum_calc += buf[i] & 0xFF;
+    }
+
+    // get packet chechsum
+    uint16_t checksum_orig = 0;
+    memcpy(&checksum_orig, &buf[checksum_index], sizeof(checksum_orig));
+
+    // check match and return result
+    if (checksum_calc != checksum_orig) {
+      NODELET_INFO("(svis_ros) checksum error [%02X, %02X] [%02X, %02X]",
+                   buf[checksum_index] & 0xFF, buf[checksum_index + 1] & 0xFF, checksum_calc, checksum_orig);
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  void GetHeader(std::unique_ptr<char> buf, header_packet &header) {
+    ind ind = 0;
+    
+    // send_count
+    header.send_count = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+    NODELET_INFO("(svis_ros) send_count: [%i, %i]", ind, header.send_count);
+    ind += 2;
+
+    // imu_packet_count
+    header.imu_count = 0xFF & buf[ind];
+    NODELET_INFO("(svis_ros) imu_packet_count: [%i, %i]", ind, header.imu_count);
+    ind++;
+
+    // strobe_packet_count
+    header.strobe_count = 0xFF & buf[ind];
+    NODELET_INFO("(svis_ros) strobe_packet_count: [%i, %i]", ind, header.strobe_count);
+    ind++;
+  }
+
+  void GetIMU(std::unique_ptr<char> buf, header_packet &header, std::vector<imu_packet> &imu_packets) {
+    for (int i = 0; i < header.imu_count; i++) {
+      imu_packet imu;
+      int ind = imu_index[i];
+
+      // timestamp
+      memcpy(&imu.timestamp, &buf[ind], 4);
+      NODELET_INFO("(svis_ros) imu.timestamp: [%i, %i]", ind, imu.timestamp);
+      ind += 4;
+
+      // accel
+      imu.acc[0] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+      imu.acc[1] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+      imu.acc[2] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+
+      NODELET_INFO("(svis_ros) imu.acc: [%i, %i, %i]", imu.acc[0], imu.acc[1], imu.acc[2]);
+
+      // gyro
+      imu.gyro[0] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+      imu.gyro[1] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+      imu.gyro[2] = 0xFFFF & (0xFF & buf[ind] | ((0xFF & buf[ind + 1]) << 8));
+      ind += 2;
+
+      NODELET_INFO("(svis_ros) imu.gyro: [%i, %i, %i]", imu.gyro[0], imu.gyro[1], imu.gyro[2]);
+
+      // save packet
+      imu_packets[i] = imu;
+    }
+  }
+
+  void GetStrobe(std::unique_ptr<char> buf, header_packet &header, std::vector<strobe_packet> &strobe_packets) {
+    for (int i = 0; i < header.strobe_count; i++) {
+      strobe_packet strobe;
+      int ind = strobe_index[i];
+      
+      // timestamp
+      memcpy(&strobe.timestamp, &buf[ind], 4);
+      NODELET_INFO("(svis_ros) strobe.timestamp: [%i, %i]", ind, strobe.timestamp);
+      ind += 4;
+
+      // count
+      strobe.count = 0xFF & buf[ind];
+      NODELET_INFO("(svis_ros) strobe.count: [%i, %i]", ind, strobe.count);
+      ind++;
+
+      // save packet
+      strobe_packets[i] = strobe;
+    }
+  }
+
+  void PrintBuffer(int num, std::unique_ptr<char> buf) {
+    NODELET_INFO("(svis_ros) buffer: ");
+    for (int i = 0; i < num; i++) {
+      printf("%02X ", buf[i] & 255);
+    }
+    printf("\n");
+  }
+  
   // hid usb packet sizes
   const int imu_data_size = 6;  // (int16_t) [ax, ay, az, gx, gy, gz]
   const int imu_buffer_size = 10;  // store 10 samples (imu_stamp, imu_data) in circular buffers
@@ -224,6 +260,8 @@ class SVISNodelet : public nodelet::Nodelet {
     int acc[3];  // units?
     int gyro[3];  // units?
   };
+
+  bool print_buffer_ = false;
 };
 
 }  // namespace svis_ros
